f598c87650384eea452f02ad6a6f01ba1af3658a
huabingood <huabingood@qq.com>
2018-08-01 16:27:01 +0800

    <feat><shellAndSQL>:测试java的if条件

--- myTest/src/main/java/myThread/MyThreadJoin.java
+++ myTest/src/main/java/myThread/MyThreadJoin.java
@@ -3,6 +3,7 @@ package myThread;
 /**
  * join 是指所有的线程等待join线程执行完毕后再执行，有点顺序执行的味道
  * join命令必须在start之后，因为只有线程启动了，才能通知到是否等待该线程执行完毕
+ * join通常是主线程等待该子线程完成，只是主线程在等待，与此同时已经启动的主线程是不受影响的
  *
  */
 public class MyThreadJoin extends Thread {
---
+++ myTest/src/main/java/practise/IfNoBound.java
@@ -0,0 +1,14 @@
+package practise;
+
+public class IfNoBound {
+    public static void main(String[] args){
+        int i=10;
+        if(i<100){
+            System.out.println("i<100");
+        }else if(i>0){
+            System.out.println("i>0");
+        }else {
+            System.out.println("aaaaa");
+        }
+    }
+}
