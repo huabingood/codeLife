fa0992da6d27db8e625655c758adef3fb60ce7bf
huabingood <huabingood@qq.com>
2018-08-10 18:46:31 +0800

    <feat><shellAndSQL>:一些小测试

---
+++ myTest/src/main/java/practise/Split.java
@@ -0,0 +1,14 @@
+package practise;
+
+public class Split {
+    public static void main(String[] args){
+        String chinatypes=args[0];
+
+
+
+        if(chinatypes.contains("\\.")){
+
+        }
+
+    }
+}
