d1502c7c8c33d31bc187dd2f8602c322a3b48977
huabingood <huabingood@qq.com>
2018-08-17 18:49:39 +0800

    <feat><shellAndSQL>:一些小测试

---
+++ myTest/src/test/java/practise/Test1.java
@@ -0,0 +1,15 @@
+package practise;
+
+public class Test1 {
+    public static void main(String[] args){
+        String str = "abc";
+
+        StringBuffer sb = new StringBuffer("huabingood");
+        System.out.println(sb.substring(1,2));
+
+        StringBuilder stringBuilder = new StringBuilder("hhhhhhhhh");
+        System.out.println(stringBuilder.substring(1,2));
+
+    }
+    
+}
