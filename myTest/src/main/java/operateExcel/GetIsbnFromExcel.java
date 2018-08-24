b476fda26411582cbc5812d5dbab2ea92fa2a2f9
huabingood <huabingood@qq.com>
2018-08-24 17:05:46 +0800

    <feat><shellAndSQL>:java操作excel,并验证isbn对应的图片是否存在

--- myTest/src/main/java/operateExcel/GetIsbnFromExcel.java
+++ myTest/src/main/java/operateExcel/GetIsbnFromExcel.java
@@ -2,13 +2,20 @@ package operateExcel;
 
 import org.apache.poi.ss.usermodel.*;
 
+import javax.swing.*;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class GetIsbnFromExcel {
     public static void main(String[] args) throws Exception{
         String fatherPath = "C:\\Users\\huabingood\\Desktop\\增量图书数据";
         String excelName = "20180718新.xlsx";
-        String imgPath = "20170719";
+        // String excelName = "sh20180805.xlsx";
+        String imgPath = "20180813";
+        //HashMap<String,Integer> hashMap = new HashMap<String, int>();
+        Map<String ,Integer> map = new HashMap<String ,Integer>();
 
         Workbook wb = null;
 
@@ -17,21 +24,41 @@ public class GetIsbnFromExcel {
         Sheet sheet = wb.getSheetAt(0);
 
         int line = sheet.getFirstRowNum();
+        int lastline = sheet.getLastRowNum();
 
-        for(;line<=1032;line++){
+        for(;line<=lastline;line++){
             Row row = sheet.getRow(line);
             Cell cell = row.getCell(1);
             cell.setCellType(Cell.CELL_TYPE_STRING);
             String isbn = cell.getStringCellValue();
 
-            String img = imgPath+File.separator+isbn+".jpg";
-
-            File f = new File(img);
-            if(f.exists()){
-                System.out.println(f.getAbsolutePath());
-            }else{
-                System.out.println("找不到文件："+f.getAbsolutePath());
+            if(null!=map){
+                if(map.containsKey(isbn)){
+                    int value = map.get(isbn);
+                    map.put(isbn,value+1);
+                }else{
+                    map.put(isbn,1);
+                }
             }
         }
+
+        Set<Map.Entry<String,Integer>> es = map.entrySet();
+
+        for(Map.Entry<String,Integer> e:es){
+            Integer v = e.getValue();
+            String k = e.getKey();
+            if(v>1){
+                String img = imgPath+File.separator+k+".jpg";
+
+                File f = new File(img);
+                if(f.exists()){
+                    System.out.println(f.getAbsolutePath());
+                }else{
+                    System.out.println("找不到文件："+f.getAbsolutePath());
+                }
+            }
+
+        }
+
     }
 }
