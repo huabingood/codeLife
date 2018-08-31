1bc2202211aaf377dc78af1ebc39865f139c5d4a
huabingood <huabingood@qq.com>
2018-08-31 16:28:54 +0800

    feat:创建一个图书类
    1.仅供学习测试

---
+++ myTest/src/main/java/oop/Books.java
@@ -0,0 +1,53 @@
+package oop;
+
+/**
+ * 创建一个图书类，包含：书名 作者 价格
+ *
+ */
+public class Books {
+    private String name;
+    private String author;
+    private double price;
+
+    public Books() {
+    }
+
+    public Books(String name, String author, double price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Books{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
