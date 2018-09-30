8bd23056e96cd5e3175c84c184b45f1ba5448531
huabingood <huabingood@qq.com>
2018-09-30 12:06:42 +0800

    feat:C languafe basic

---
+++ CDSAlgorithm/CPractise/basicMy/Demo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+# define PI 3.14
+
+main(){
+    double r;
+    printf("Input r:");
+    scanf("%lf",&r);
+
+    printf("circumference = %f\n",2*PI*r);
+
+}
---
+++ myTest/src/main/java/practise/Demo1.java
@@ -0,0 +1,7 @@
+package practise;
+
+public class Demo1 {
+    public static void main(String[] args){
+        System.out.print(3/4);
+    }
+}
