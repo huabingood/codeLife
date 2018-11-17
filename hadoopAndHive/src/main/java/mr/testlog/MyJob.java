ea6b1b685db75deaf176d83feec0ca40e84e1062
huabingood <huabingood@qq.com>
2018-11-17 14:58:45 +0800

    feat:统计流量

---
+++ hadoopAndHive/src/main/java/mr/testlog/MyJob.java
@@ -0,0 +1,38 @@
+package mr.testlog;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import utiles.HDFSCheck;
+
+import java.io.IOException;
+
+public class MyJob {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf);
+
+        Path outPath = new Path("/output/o1");
+        HDFSCheck.ifExistRm(conf,outPath);
+
+        job.setJobName("TestLog");
+        job.setMapperClass(MyMap.class);
+        job.setReducerClass(MyReduce.class);
+
+        job.setMapOutputValueClass(Text.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        FileInputFormat.setInputPaths(job,new Path("/input/test/test.log"));
+        FileOutputFormat.setOutputPath(job,outPath);
+
+        boolean res = job.waitForCompletion(true);
+        System.out.println(res?"success":"failed");
+
+    }
+}
--- hadoopAndHive/src/main/java/mr/testlog/MyMap.java
+++ hadoopAndHive/src/main/java/mr/testlog/MyMap.java
@@ -1,4 +1,16 @@
 package mr.testlog;
 
-public class MyMap {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class MyMap extends Mapper<Text, Text,Text,Text> {
+    @Override
+    protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+        String line = key.toString();
+        String[] words = line.split("\t");
+
+        context.write(new Text(words[1]),new Text(words[-3]+","+words[-2]));
+    }
 }
